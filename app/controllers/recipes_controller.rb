d43f7994388407c80b85179d23e29a8c94831705
Philip Oka <philip.oka@gmail.com>
2020-12-24 16:18:02 +0000

    visualização somente das próprias receitas

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: [:show, :edit, :update, :destroy]
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.where(user: current_user)
   end
 
   def show
