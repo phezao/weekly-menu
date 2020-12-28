50088d166f9cf4db453f0e33f1bced9c8a25fb09
Philip <philip.oka@gmail.com>
2020-12-28 15:45:16 +0000

    gerador de menu funcionando, criou um post request para gerar o novo cardapio (#12)

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -3,7 +3,6 @@ import 'select2';
 import '../../../node_modules/select2/dist/css/select2.min.css'
 
 const initSelect2 = () => {
-    console.log("Hello");
     $(".js-example-tags").select2({
         tags: true
     });
