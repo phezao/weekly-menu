778fe2fc763db8a31095b5fbfc8d425bb86be533
Philip <philip.oka@gmail.com>
2020-12-28 16:27:34 +0000

    novo controller para a lista de supermercado, funcionando com todas as receitas preenchidas (#14)

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,7 +1,7 @@
 class Menu < ApplicationRecord
-  has_many :recipes, through: :menu_recipes
-  accepts_nested_attributes_for :recipes
   has_many :menu_recipes
   accepts_nested_attributes_for :menu_recipes
+  has_many :recipes, through: :menu_recipes
+  accepts_nested_attributes_for :recipes
   belongs_to :user
 end
