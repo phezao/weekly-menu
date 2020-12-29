38690392b2b04c5b0d6ac010c27085d16be92eb7
Philip Oka <philip.oka@gmail.com>
2020-12-29 00:45:24 +0000

    permitir o usuario de destruir a receita e a ligação receita_ingrediente

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ApplicationRecord
 belongs_to :user
 has_many :menus, through: :menu_recipes
 has_many :menu_recipes
-has_many :recipe_ingredients
+has_many :recipe_ingredients, dependent: :destroy
 has_many :ingredients, through: :recipe_ingredients
 
 
